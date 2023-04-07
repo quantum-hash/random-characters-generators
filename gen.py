d83357f21be85b367789b2449e917bb602dffd3f
AB <96754248+quantum-hash@users.noreply.github.com>
2023-04-07 18:30:49 +0200

    Update gen.py

--- gen.py
+++ gen.py
@@ -5,9 +5,9 @@ letters = ["a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","
 numbers = ["0","1","2","3","4","5","6","7","8","9"]
 
 
-def generate():
+def generate(lenght=12):
 	x = [*random.choice(specials), *random.sample([*random.choices(letters, k=6), *random.choices(numbers, k=2)], len([*random.choices(letters, k=6), *random.choices(numbers, k=2)])), *random.choice(specials), *random.choice(numbers), *random.choice(specials)]
-	fi = str("").join(x)
+	fi = str("").join(x)[:int(lenght)]
 	return("key : "+fi+"\n"+"len : "+str(len(fi)))
 
 
